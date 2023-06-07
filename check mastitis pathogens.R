4cfcbd3a5fa4bc992cd68d6be9eba6dfb9a76892
Yale Deng <120678988+yale-deng@users.noreply.github.com>
2023-06-07 13:56:19 -0500

    Figure 8_mastitis_pathogens.R

--- check mastitis pathogens.R
+++ check mastitis pathogens.R
@@ -4,15 +4,15 @@ library(phyloseq)
 library(microbiome)
 library(patchwork)
 
-setwd("~/Desktop/Projects/01.Cow_Milk_Microbiome/Analysis")
+# set file path
 
-path.rds <- ("RDS")
-path.figures <- ("Figures/")
+path.rds <- ("Analysis/RDS")
+path.figures <- ("Analysis/Figures")
 
 # mastitis pathogens
 pathogens <- c("Streptococcus", "Staphylococcus", "Mycoplasma", "Prototheca", "Aerococcus", "Enterococcus", "Lactococcus", "Micrococcus", "Trueperella", "Bacillus", "Corynebacterium", "Listeria", "Acinetobacter", "Aeromonas", "Citrobacter", "Enterobacter", "Escherichia/Shigella", "Flavimonas", "Hafnia", "Klebsiella", "Pantoea", "Plesimonas", "Proteus", "Pseudomonas", "Salmonella", "Serrati", "Serratia", "Stenotrophomonas", "Yersinia", "Yeast", "Nocardia")
 
-ps.sourcetracker <- readRDS(file.path(path.rds, "ps.decontam.st.checked.rds"))
+ps.sourcetracker <- readRDS(file.path(path.rds, "ps.decontam.st.rds"))
 
 
 # change ps in relative abundance
@@ -38,59 +38,4 @@ p.melt.mast <- ggplot(melt.mast, aes(x = X.SampleID, y = Abundance, fill = Genus
 
 print(p.melt.mast)
 
-
-ggsave (file.path(path.figures, "Figure 8. Potential mastitis abundance per sample_checked.png"), p.melt.mast, dpi = 600)
-
-
-
-
-# plot grouped relative abundance of mastitis
-p.mast.grouped <- plot_composition(ps.mast.gen.rel, sample.sort = NULL,
-                                   average_by = "Type",
-                                   otu.sort = NULL,
-                                   plot.type = "barplot",
-                                   verbose = FALSE) +
-  xlab("") +
-  ylab("Relative abundance")+
-  theme_bw() + 
-  theme(axis.text.x = element_text(angle = -45),
-        axis.ticks.x=element_blank())+
-  scale_fill_brewer("Genus", palette = "Paired")
-print(p.mast.grouped)
-
-# check Staphylococcus positive samples from CM, get the seq for BLAST
-ps.sty.positive <- subset_samples(ps.sourcetracker, X.SampleID %in% c("MS56_S48", "MS35_S3"))
-ps.sty.positive <- prune_taxa(taxa_sums(ps.sty.positive)>0, ps.sty.positive)
-taxa.sty.positive <- tax_table(ps.sty.positive) %>% data.frame() %>% subset(Genus == ("Staphylococcus"))
-seq.sty.positive <- rownames(taxa.sty.positive)
-
-taxa <- readRDS(file.path(path.rds, "taxa.rds")) %>% data.frame()
-taxa$seq <- paste0("Seq", seq(dim(taxa)[1]))
-taxa.sty.seq <- subset(taxa, taxa$seq %in% seq.sty.positive) %>% rownames()
-write.csv (taxa.sty.seq, "results/sequence.staphylococcus.culture.positive.csv")
-
-# get the mean of abundance by Genus
-mean.mast.rel <- melt.mast %>% 
-  group_by(Genus) %>% 
-  summarise (genus_mean = mean(Abundance)*100) %>% 
-  arrange(desc(genus_mean))
-mean.mast.rel
-
-# get the mean of abundance by Type
-mean.mast.rel.type <- melt.mast %>% 
-  group_by(Type) %>% 
-  summarise (type_mean = mean(Abundance)*100) %>% 
-  arrange(desc(type_mean))
-mean.mast.rel.type
-
-# get the mean of abundance by Genus and Type
-mean.mast.rel.grouped <- melt.mast %>% 
-  group_by(Genus,Type) %>% 
-  summarise (genus_mean = mean(Abundance)*100)
-mean.mast.rel.grouped$genus_mean <- round(mean.mast.rel.grouped$genus_mean, digits=1)
-mean.mast.rel.grouped
-
-# merge the plots
-p.mast <- (p.melt.mast | p.mast.grouped) + plot_layout(widths = c(3, 1)) + plot_annotation(tag_levels = c("A"))
-p.mast
-ggsave (file.path(path.figures, "Figure 8. Potential mastitis abundance per sample.png"), p.melt.mast, dpi = 600)
+ggsave (file.path(path.figures, "Figure 8. Potential mastitis abundance.png"), p.melt.mast, dpi = 600)
